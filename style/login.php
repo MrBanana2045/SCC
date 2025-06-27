0cdbf365e6d485e2b8aa09e3e0e5744f6faeb61e
MrSaad <mrbananatyphon2020@gmail.com>
2025-06-27 16:06:21 +0330

    Create login.php

---
+++ style/login.php
@@ -0,0 +1,69 @@
+@import url('https://fonts.googleapis.com/css?family=WDXL%20Lubrifont%20TC:700|WDXL%20Lubrifont%20TC:400');
+html{
+  -webkit-user-select: none; /* Safari */
+  -ms-user-select: none; /* IE 10 and IE 11 */
+  user-select: none; /* Standard syntax */
+}
+body{
+    background: #0d1614;
+    font-family: 'WDXL Lubrifont TC';
+    color:#e4ecea;
+    padding: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+}
+form{
+    background: #152521;
+    padding: 20px;
+    border-radius: 20px;
+    padding: 20px;
+    box-shadow: 0 0 10px rgba(0,0,0,0.1);
+    width: 300px;
+}
+  label {
+    display: block;
+    text-align: left;
+    margin-bottom: -10px;
+    font-weight: bold;
+  }
+  input[type="text"],
+  input[type="password"] {
+    width: 100%;
+    padding: 8px 10px;
+    margin-bottom: 15px;
+    box-sizing: border-box;
+    border: 2px solid #e4ecea;
+    outline: none;
+    font-size: 20px;
+    border-radius: 4px;
+    background: none;
+    color:white;
+    font-family: 'WDXL Lubrifont TC';
+  }
+
+  input[type="submit"] {
+    width: 48%;
+    padding: 10px;
+    background-color: #214b42;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-family: 'WDXL Lubrifont TC';
+    font-size: 20px;
+  }
+  input[type="submit"]:hover {
+    background-color: #15312b;
+  }
+  button {
+    width: 48%;
+    padding: 10px;
+    background-color: #214b42;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-family: 'WDXL Lubrifont TC';
+    font-size: 20px;
